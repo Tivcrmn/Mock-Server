d167b6f25c02226e18ff3dfbf8fe8daf43c48ae1
Tivcrmn <zhangjitao0405@gmail.com>
2019-05-26 19:59:39 -0700

    add 404 page

--- client/src/pages/F0F/index.js
+++ client/src/pages/F0F/index.js
@@ -1,9 +1,38 @@
 import React, { Component } from "react";
+import image from "assets/404.jpg";
+import { withRouter } from "react-router-dom";
 
 class F0F extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 5,
+    };
+  }
+
+  componentDidMount() {
+    this.timer = setInterval(() => {
+      this.setState((state, props) => ({
+        counter: state.counter - 1,
+      }));
+    }, 1000);
+    setTimeout(() => {
+      this.props.history.push("/");
+    }, 5000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
   render() {
-    return <h1>you are losing now</h1>;
+    return (
+      <div>
+        <img src={image} alt="" />
+        <h2>After { this.state.counter } s you will go to dashboard</h2>
+      </div>
+    );
   }
 }
 
-export default F0F;
+export default withRouter(F0F);
